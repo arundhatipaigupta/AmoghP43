95fc364873d52f1a79fa490e20fd08ab9245c838
arundhatipaigupta <74766490+arundhatipaigupta@users.noreply.github.com>
2021-03-22 08:31:01 +0530

    Update sketch.js
    updated angle in degrees

--- sketch.js
+++ sketch.js
@@ -9,6 +9,9 @@ var scAngle = map(sc,0,60,0,360);
 function setup() {
   createCanvas(800,400);
  // createSprite(400, 200, 50, 50);
+  
+  //To use angle in Degrees
+    angleMode(DEGREES);   
 }
 
 function draw() {
@@ -17,7 +20,7 @@ function draw() {
   rotate(scAngle);
 stroke(255,0,0);
 strokeWeight(7);
-line(400,200,0,0);
+line(0,0,100,0);
 pop();
   drawSprites();
 }
